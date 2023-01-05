1554214d1188803a9c04d12bcd268f3af337baa2
Sergey <rajkovsergej81@gmail.com>
2023-01-05 13:22:47 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return (n > 0 && n % 10 === 0) || (n > 0 && n % 10 != 0) ? Number(n.toString().split("").reverse().join("")) : Number((n * -1).toString().split("").reverse().join(""));
+
+    
+};
